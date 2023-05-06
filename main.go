9d97cb71db8474f5f21b0bb09dfca9b3d59fac73
phillychi3 <phillychi3@gmail.com>
2023-05-07 01:04:02 +0800

    finish

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"log"
 	"net/http"
 	"os"
@@ -43,34 +45,38 @@ func main() {
 				switch message := event.Message.(type) {
 				case *linebot.TextMessage:
 					msgtext := message.Text
-					//graphql
+					if len(msgtext) < 20 {
+						break
+					}
 
 					request := `
 					query {
 						ListArticles(filter: {
 							moreLikeThis: {
-							like: "<replace>"
+								like: "<replace>"
 							}
 						}) {
 							edges {
-							node {
-								id
-								text
-								articleType
-								createdAt
-								articleReplies{
-								reply{
+								node {
+									id
 									text
+									articleType
+									createdAt
+									articleReplies{
+										reply{
+											type
+											text
+										}
+									}
+									
 								}
-								}
-								
-							}
 							}
 							totalCount
 						}
 					}
 				`
 					msgtext = strings.Replace(msgtext, "\n", "", -1)
+					msgtext = strings.Replace(msgtext, "\"", "", -1)
 					request = strings.Replace(request, "<replace>", msgtext, 1)
 
 					req := graphql.NewRequest(request)
@@ -87,6 +93,7 @@ func main() {
 									CreatedAt      string
 									ArticleReplies []struct {
 										Reply struct {
+											Type string
 											Text string
 										}
 									}
@@ -99,9 +106,74 @@ func main() {
 						log.Fatal(err)
 					}
 					log.Println(respData)
+					if respData.ListArticles.TotalCount != 0 && respData.ListArticles.Edges[0].Node.ArticleReplies[0].Reply.Type == "RUMOR" {
+						// creage a response to https://api.pawan.krd/v1/chat/completions
+						log.Print("檢測可能為不實資訊，系統產生報告中...")
+						data := (`
+						{
+							"model": "gpt-3.5-turbo",
+							"max_tokens": 600,
+							"messages": [
+								{
+									"role": "system",
+									"content": "請幫我以第三者的角度去客觀的解釋這個問題，使用100字，並在最後給出結論"
+								},
+								{
+									"role": "user",
+									"content": "<replace>"
+								}
+							]
+						}
+						`)
+						artext := respData.ListArticles.Edges[0].Node.ArticleReplies[0].Reply.Text
+						artext = strings.Replace(artext, "\n", "", -1)
+						artext = strings.Replace(artext, "\"", "", -1)
+						message := "謠言:" + msgtext + "。" + "解釋:" + artext
+						data = strings.Replace(data, "<replace>", message, 1)
+						req, err := http.NewRequest("POST", "https://api.pawan.krd/v1/chat/completions", bytes.NewBuffer([]byte(data)))
+						if err != nil {
+							log.Fatal(err)
+						}
+						req.Header.Add("Authorization", "Bearer "+os.Getenv("PAWAN_TOKEN"))
+						req.Header.Add("Content-Type", "application/json")
+						client := &http.Client{}
+						resp, err := client.Do(req)
+						if err != nil {
+							log.Fatal(err)
+						}
+						defer resp.Body.Close()
 
-					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
-						log.Print(err)
+						type bodys struct {
+							ID      string `json:"id"`
+							Created int64  `json:"created"`
+							Model   string `json:"model"`
+							Object  string `json:"object"`
+							Choices []struct {
+								FinishReason string `json:"finish_reason"`
+								Index        int    `json:"index"`
+								Message      struct {
+									Content string `json:"content"`
+									Role    string `json:"role"`
+								} `json:"message"`
+							} `json:"choices"`
+							Usage struct {
+								PromptTokens     int `json:"prompt_tokens"`
+								CompletionTokens int `json:"completion_tokens"`
+								TotalTokens      int `json:"total_tokens"`
+							} `json:"usage"`
+						}
+
+						body := bodys{}
+						err = json.NewDecoder(resp.Body).Decode(&body)
+						if err != nil {
+							log.Fatal(err)
+						}
+						log.Println(body)
+
+						if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(body.Choices[0].Message.Content)).Do(); err != nil {
+							log.Print(err)
+						}
+
 					}
 				default:
 					log.Printf("not text message: %v", message)
